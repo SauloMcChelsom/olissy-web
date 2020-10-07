519e5cf19c712f2c228ab84ff2318fd26e67d617
saulo <you@example.com>
2020-10-06 22:34:36 -0300

    user finalizado

---
+++ src/app/page/client/column/column-client/column-client.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-column-client',
+  templateUrl: './column-client.component.html',
+  styleUrls: ['./column-client.component.css']
+})
+
+export class ColumnClientComponent {}
